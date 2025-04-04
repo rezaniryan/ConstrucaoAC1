22c9ea9430ebb1e93504caaaf74209d9ea335ec6
Ryan Rezani <cancelaryanrezani@gmail.com>
2025-04-04 19:41:41 -0300

    Update NotaAluno.java

--- NotaAluno.java
+++ NotaAluno.java
@@ -4,21 +4,21 @@ public class NotaAluno {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
 
-        System.out.print("Digite a nota minima para aprovo: ");
+        System.out.print("Digite a media: ");
         double notaMinima = scr.nextDouble();
 
         System.out.print("Digite a nota da AC1: ");
         double ac1 = scr.nextDouble();
         System.out.print("Digite a nota da AC2: ");
         double ac2 = scr.nextDouble();
-        System.out.print("Digite a nota da AC3: ");
-        double ac3 = scr.nextDouble();
+        System.out.print("Digite a nota da AF: ");
+        double af = scr.nextDouble();
 
         verificarNota("AC1", ac1, notaMinima);
         verificarNota("AC2", ac2, notaMinima);
-        verificarNota("AC3", ac3, notaMinima);
+        verificarNota("AF", af, notaMinima);
 
-        double media = (ac1 + ac2 + ac3) / 3;
+        double media = (ac1 + ac2 + af) / 3;
         System.out.printf("\nMedia final: %.2f\n", media);
 
         if (media >= notaMinima) {
